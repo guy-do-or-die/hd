bd3faa63cfbadb1df50fbd1d9f10e76afb0452b7
Egor Gusev <guy.do.or.die@gmail.com>
2016-02-03 23:25:32 +0300

    rainbow&sort

--- sol.py
+++ sol.py
@@ -1,4 +1,7 @@
 from PIL import Image, ImageDraw
+from collections import OrderedDict
+from itertools import islice
+import random
 
 XS, Y0 = 1000, 800
 
@@ -23,6 +26,7 @@ for rline in iter(raw_input, '====='):
         mem[pin] = xy
         draw.ellipse(((x - 2, Y0 - y - 2), (x + 2, Y0 - y + 2)), fill='blue', outline='blue')
 
+conns = {}
 
 for i, rline in enumerate(iter(raw_input, '#####')):
     line = rline.split(';')
@@ -31,11 +35,34 @@ for i, rline in enumerate(iter(raw_input, '#####')):
     n1x, n1y = mcu[n1]
     n2x, n2y = mem[n2]
 
-    draw.line(((n1x, Y0 - n1y), (n1x, Y0 - n2y)), fill='green', width=1)
-    draw.line(((n1x, Y0 - n2y), (n2x, Y0 - n2y)), fill='green', width=1)
+    '''
+    c1 = n1x * n1y
+    c2 = (n1x + n2x) * (n1y + n2y)
+    '''
+
+    '''
+    c1 = n1x - n2x
+    c2 = n1y - n2y
+    '''
+
+    c1 = n2x
+    c2 = n1y
+
+    conns[abs(c1 * c2)] = (n1x, n1y, n2x, n2y)
 
     if i > 100:
         break
+
+sconns = OrderedDict(sorted(conns.items()))
+
+for i in islice(sconns, 10):
+    n1x, n1y, n2x, n2y = conns[i]
+
+    color = '#%06x' % random.randint(0, 0xFFFFFF)
+
+    draw.line(((n1x, Y0 - n1y), (n1x, Y0 - n2y)), fill=color, width=1)
+    draw.line(((n1x, Y0 - n2y), (n2x, Y0 - n2y)), fill=color, width=1)
+    draw.line(((n1x, Y0 - n1y), (n2x, Y0 - n2y)), fill='white', width=1)
 
 
 del draw
