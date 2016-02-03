df5c5bb4233f882f9330941f978e81479f7cf4b7
Egor Gusev <guy.do.or.die@gmail.com>
2016-02-03 12:15:03 +0300

    pretty

--- sol.py
+++ sol.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw
 
-image = Image.new("RGBA", (800, 800), (0, 0, 0, 0))
+image = Image.new('RGBA', (800, 800), (0, 0, 0, 0))
 draw = ImageDraw.Draw(image)
 
 mem = {}
@@ -10,19 +10,19 @@ for rline in iter(raw_input, '====='):
     line = rline.split(';')
 
     pin, t, xy = (int(line[0]), int(line[1]),
-                  map(lambda a: float(a.replace(',', '.')) * 40, line[2:]))
-
-    if t == 2:
-        mem[pin] = tuple(xy)
-    else:
-        mcu[pin] = tuple(xy)
+                  tuple([150 + float(a.replace(',', '.')) * 30 for a in line[2:]]))
 
     x, y = xy
-    draw.ellipse(((x - 5, y - 5), (x + 5, y + 5)), fill='red', outline='red')
 
-i = 0
-for rline in iter(raw_input, '#####'):
-    i += 1
+    if t == 1:
+        mcu[pin] = xy
+        draw.ellipse(((x - 2, y - 2), (x + 2, y + 2)), fill='red', outline='red')
+    else:
+        mem[pin] = xy
+        draw.ellipse(((x - 2, y - 2), (x + 2, y + 2)), fill='blue', outline='blue')
+
+
+for i, rline in enumerate(iter(raw_input, '#####')):
     line = rline.split(';')
     n1, n2 = map(int, line)
 
@@ -31,8 +31,10 @@ for rline in iter(raw_input, '#####'):
 
     draw.line(((n1x, n1y), (n1x, n2y)), fill='green', width=1)
     draw.line(((n1x, n2y), (n2x, n2y)), fill='green', width=1)
+
     if i > 100:
         break
+
 
 del draw
 image.show()
